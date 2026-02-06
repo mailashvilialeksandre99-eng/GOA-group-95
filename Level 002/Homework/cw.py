35acd08b5237cf0f441b7c8d28463b1ff599eb98
mailashvilialeksandre99-eng <mailashvilialeksandre99@gmail.com>
2026-02-06 19:22:12 +0000

    second commit

---
+++ Level 002/Homework/cw.py
@@ -0,0 +1,39 @@
+number1 = 20
+number2 = 4
+
+print (number1 / number2)
+print (number1 * number2)
+print (number1 + number2)
+print (number1 - number2)
+print (number1 % number2)
+
+
+
+birth_year = 2011
+current_year = 2026
+age = birth_year - current_year
+
+print (age)
+
+# variables are dynamically created when you assign a value to them
+# example:
+#   last_name = "Mailashvili" this is declaration
+
+
+# In Python, initialization refers to the process of assigning an 
+# initial value to a variable or setting up an object when it is created
+#  example:
+#  x = 0
+#  y = 0.0
+
+
+# reainitilize means when a new value is assigned to the variable like lets say age = 15 if you put another variable after
+# like age = 19 it will change its value to 19 as it has changed its information to the new one
+
+
+# In Python, concantenation is used for string concatenation within the print() 
+# function or anywhere else you need to join strings
+# example:
+#  name = "aleksandre"
+#  last_name = "mailashvili"
+#   print (name +" "+ last_name) (concantenation is used for joining up 2 strings together)
