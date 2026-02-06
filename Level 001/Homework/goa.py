0b3c9cbd306618f11479e0f95933faf483c30814
mailashvilialeksandre99-eng <mailashvilialeksandre99@gmail.com>
2026-02-06 18:27:25 +0000

    first commit

---
+++ Level 001/Homework/goa.py
@@ -0,0 +1,14 @@
+name = ("Aleksandre")
+age = 14
+address = ("Flintshire")
+height = 5.9
+weight = 85
+
+print (name)
+print (age)
+print (address)
+print (height)
+print (weight)
+
+# print = ექშენი რომელიც გამოიყენება რომ ცვლადი გამოიტანო ტერმინალზე/ეკრანზე
+# variable/ცვლადი
---
+++ Level 002/Classwork/cw.py
@@ -0,0 +1,6 @@
+name = "ალექსანდრეს"
+action = "უყვარს"
+action2 = "კომპიუტერული"
+end = "დავალება"
+
+print (name + " "+ action + " " + action2 + " " + end)
