7af665c6648a931a9c980a688e56b9f1b0a23ea9
mailashvilialeksandre99-eng <mailashvilialeksandre99@gmail.com>
2026-02-20 19:25:13 +0000

    second commit

---
+++ Level 006/Classwork/task.py
@@ -0,0 +1,7 @@
+# dog_name = input('Please enter your dogs name')
+
+# print (type(dog_name))
+
+birth_date = int(input('Please enter your birth_date'))
+
+print (2026 - birth_date)
---
+++ Level 006/Homework/task.py
@@ -0,0 +1,50 @@
+#  nomer 1
+# these types are needful for reading the codes more easily
+
+
+# nomer 2
+
+# name = input('Please enter name')
+
+# print (name)
+
+
+# # # nomer 3
+# # string 
+# name = type(input('Please enter name'))
+
+# print(name)
+
+# # integer
+# age = type(int(input('Please enter age')))
+
+# print(age)
+
+# # float
+# number = type(float(input('Please enter number')))
+
+# # print(number)
+
+# #  nomer 4
+
+# length = float('Please enter length')
+
+# print(length)
+# # ვერ ვიცი როგორ
+
+# # nomer 5
+
+# number = int(input('enter number'))
+# age = int(input('enter age'))
+
+# print(age+number)
+# print(age / number)
+# print(age * number)
+# print(age - number)
+
+# nomer 6
+
+height = float(input('Enter height'))
+weight = float(input('Enter weight'))
+
+print(weight / height * height)
