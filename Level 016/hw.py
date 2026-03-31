411beedf188d91de33ffe09a0409d5ea6cab78e4
mailashvilialeksandre99-eng <mailashvilialeksandre99@gmail.com>
2026-03-31 15:53:19 +0100

    second commit

---
+++ Level 016/hw.py
@@ -0,0 +1,34 @@
+# # nomer 1
+# for i in range (0,101):
+#     print(i)
+
+# # nomer 2
+# # for i in range (40,251,2):
+# #     print(i)
+
+# # nomer 3 
+
+# i = 200
+
+# while i <= 400:
+#     if i % 4 == 0 and i % 5 == 0:
+#         print(i)
+#     i += 1
+
+# nomer 4
+
+num = int(input("Enter a number: "))
+
+i = 2
+
+while i <= num:
+    print(i)
+    i += 1
+
+# nomer 5
+
+num = int(input("Enter a number: "))
+
+while num >= 0:
+    print(num)
+    num -= 1
--- level 014/homework/hw2.py
+++ level 014/homework/hw2.py
@@ -1,28 +1,28 @@
-# nomer 4
-for i in range (25):
-    print('For Loop')
+# # nomer 4
+# for i in range (25):
+#     print('For Loop')
 
-# nomer 5
+# # nomer 5
 
-for i in range (25,76):
-    print(i)
+# for i in range (25,76):
+#     print(i)
 
-# nomer 6
+# # nomer 6
 
-for i in range (45,91):
-    print(i)
+# for i in range (45,91):
+#     print(i)
 
-# nomer 7
+# # nomer 7
 
-for i in range (25,151,4):
-    print(i)
+# for i in range (25,151,4):
+#     print(i)
 
-# nomer 8
+# # nomer 8
 
-for i in range (100,1001,10):
-    print(i)
+# for i in range (100,1001,10):
+#     print(i)
 
 # nomer 9
 
-for i in range (100):
-    print("პითონი მაგარია")
+for i in range (1,101):
+    print(i, "python is best")
