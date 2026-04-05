1a3452a4865eee6fe3296b029ed04c04fa70fc41
mailashvilialeksandre99-eng <mailashvilialeksandre99@gmail.com>
2026-04-05 13:23:03 +0100

    second commit

---
+++ Level 018/Homework/hw.py
@@ -0,0 +1,135 @@
+# nomer 1
+# for loop
+for i in range(200, 501):
+    if i % 4 == 0 and i % 7 == 0:
+        print(i)
+
+# while loop 
+
+i = 200
+while i <= 500:
+    if i % 4 == 0 and i % 7 == 0:
+        print(i)
+    i += 1
+
+# nomer 2
+
+# for loop 
+
+for i in range(300, 1001):
+    if i % 3 == 0 or i % 10 == 0:
+        print(i)
+
+# while loop
+
+i = 300
+while i <= 1000:
+    if i % 3 == 0 or i % 10 == 0:
+        print(i)
+    i += 1
+
+# nomer 3
+
+for i in range (1,51):
+    if i % 2 == 0:
+        print("even" (i))
+    else:
+        print("odd" (i))
+
+# nomer 4
+# ???
+
+# nomer 5
+
+for _ in range(1):
+    num = int(input("Enter a number: "))
+    
+    if num % 2 == 0 and num % 3 == 0:
+        print("Good")
+    elif num % 2 == 0:
+        print("Two")
+    elif num % 3 == 0:
+        print("Three")
+
+# nomer 6
+
+for i in range(1, 101):
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        print(i)
+
+# nomer 7
+
+# ???
+
+
+# nomer 8
+
+num = int(input("Enter a number: "))
+
+i = 1
+while i <= num:
+    if i % 4 != 0:
+        print(i)
+    i += 1
+
+# nomer 9
+
+# for loop
+for i in range(5):
+    num = int(input(f"Enter number {i+1}: "))
+    
+    if num % 2 == 0:
+        print("Even")
+    else:
+        print("Odd")
+
+# while loop 
+# ver mivxvdi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
