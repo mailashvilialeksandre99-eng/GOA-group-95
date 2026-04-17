74a8dbb451e6f1e07b0ae8c5bc0bb419735583b5
mailashvilialeksandre99-eng <mailashvilialeksandre99@gmail.com>
2026-04-17 16:08:53 +0100

    second commit

---
+++ Level 022/Homework/hw.py
@@ -0,0 +1,87 @@
+# nomer 1
+name = "Aleksandre"
+print(name[0])
+
+# nomer 2
+last_name = "Mailashvili"
+print(last_name[-1])
+
+# nomer 3
+text = "python"
+print(text[2])
+
+# nomer 4
+text = "Programming"
+print(text[4])
+
+# nomer 5
+text = "Computer"
+print(text[-3])
+
+# nomer 6
+text = "shaurma"
+print(text[3:7])
+
+# nomer 7
+text = "Georgia"
+print(text[2:6])
+
+# nomer 8
+word = "yumbarebi"
+
+if word[0] == "A":
+    print(word[:3])
+else:
+    print(word[-3:])
+
+# nomer 9
+text = "Education"
+print(text[1:-1])
+
+# nomer 10
+text = "Developer"
+
+result = text[:3] + text[-3:]
+print(result)
+
+# nomer 11
+sentence = "me miyvars shaurma"
+first_word = sentence.split()[0]
+print(first_word)
+
+# nomer 12
+text = "abcdef"
+print(text[::2])
+
+# nomer 13
+my_list = [1, "a", 3.5, True, "hello", 10, 20, 30]
+
+print(my_list[2:6])
+
+# nomer 14
+musicians = ["Michael Jackson", "2Pac", "50 Cent", "Miyagi", "DJ Khaled"]
+
+print(musicians[-2:])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
